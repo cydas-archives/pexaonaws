b0df5c4fff25d6c88ea2f96532ab51bde8586125
Shingo <yoshidashingo@users.noreply.github.com>
2022-11-07 10:46:07 +0900

    Initial Commit

---
+++ lib/backend-stack.ts
@@ -0,0 +1,63 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'; // L2
+import * as rds from 'aws-cdk-lib/aws-rds';
+
+export class BackendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const systemName = this.node.tryGetContext('systemName');
+    const stageName = this.node.tryGetContext('stageName');
+
+    // VPC(L2)
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      vpcName: `${systemName}-${stageName}-vpc`,
+//      cidr: '10.0.0.0/16',
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: `${systemName}-${stageName}-subnet-public`,
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: `${systemName}-${stageName}-subnet-private`,
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    })
+
+    // Raw overrides to L1 construct (Escape Hatching / Bypassing)
+    const cfnPublicSubnet1 = vpc.publicSubnets[0].node.defaultChild as ec2.CfnSubnet
+    cfnPublicSubnet1.addPropertyOverride('CidrBlock', `10.0.11.0/24`)
+    const cfnPublicSubnet2 = vpc.publicSubnets[1].node.defaultChild as ec2.CfnSubnet
+    cfnPublicSubnet2.addPropertyOverride('CidrBlock', `10.0.12.0/24`)
+    const cfnPrivateSubnet1 = vpc.publicSubnets[0].node.defaultChild as ec2.CfnSubnet
+    cfnPrivateSubnet1.addPropertyOverride('CidrBlock', `10.0.21.0/24`)
+    const cfnPrivateSubnet2 = vpc.publicSubnets[1].node.defaultChild as ec2.CfnSubnet
+    cfnPrivateSubnet2.addPropertyOverride('CidrBlock', `10.0.22.0/24`)
+
+    // RDS
+    const cluster = new rds.DatabaseCluster(this, 'Database', {
+      engine: rds.DatabaseClusterEngine.auroraMysql({ version: rds.AuroraMysqlEngineVersion.VER_2_08_1 }),
+      credentials: rds.Credentials.fromGeneratedSecret('clusteradmin'), // Optional - will default to 'admin' username and generated password
+      instanceProps: {
+        // optional , defaults to t3.medium
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.SMALL),
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        vpc,
+      },
+    });
+
+    // ECS
+
+
+  }
+}
---
+++ test/pexaonaws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Pexaonaws from '../lib/pexaonaws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pexaonaws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Pexaonaws.PexaonawsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
